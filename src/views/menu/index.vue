8910fb18df73ac513f40b18cfc62a6f6a4f36ea7
w1q2q3q <1935417616@qq.com>
2018-10-29 17:53:37 +0800

    212

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,14 +1,97 @@
 <template>
-
-
+    <el-container>
+      <el-aside width="auto" style="height: 900px">
+        <div style="height:7%;background-color: darkcyan">
+          <!--<el-radio-group v-model="isCollapse">-->
+            <!--<el-radio-button :label="false">展开</el-radio-button>-->
+            <!--<el-radio-button :label="true">收起</el-radio-button>-->
+          <!--</el-radio-group>-->
+        </div>
+        <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse" unique-opened="false" :default-openeds="openeds" >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span slot="title">首页</span>
+            </template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span slot="title">用户管理</span>
+            </template>
+            <el-menu-item index="2-1"><router-link to="/">账号管理</router-link></el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-document"></i>
+              <span slot="title">项目管理</span>
+            </template>
+            <el-menu-item index="3-1"><router-link to="project">项目管理</router-link></el-menu-item>
+          </el-submenu>
+          <el-submenu index="4" >
+            <template slot="title">
+              <i class="el-icon-document"></i>
+              <span slot="title">用例管理</span>
+            </template>
+            <el-menu-item index="4-1">用例管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-document"></i>
+              <span slot="title">任务管理</span>
+            </template>
+            <el-menu-item index="5-1">任务管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="6">
+            <template slot="title">
+              <i class="el-icon-document"></i>
+              <span slot="title">节点管理</span>
+            </template>
+            <el-menu-item index="6-1">节点管理</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header style="background-color: #faebd7">
+          <el-radio-group v-model="isCollapse">
+            <el-radio-button :label="false">展开</el-radio-button>
+            <el-radio-button :label="true">收起</el-radio-button>
+          </el-radio-group>
+        </el-header>
+        <el-main style="background-color: #f0f8ff">
+        </el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  name: 'index',
+  data () {
+    return {
+      openeds: ['2'],
+      multipleSelection: [],
+      isCollapse: false
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 93%;
+    background-color : beige;
+    text-color: #fff;
+    active-text-color: #ffd04b
+  }
+  a {
+     text-decoraction: none;
+   }
+  .router-link-active {
+    text-decoration: none;
+  }
 
 </style>
---
+++ src/views/people/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <el-row :gutter="20" style="padding-bottom: 20px;">
+    <el-col :span="16" style="padding-left: 30px">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">用户管理</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">账号管理</a></el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-col>
+  </el-row>
+  <el-row :gutter="20">
+    <el-col :span="20" :offset="1" style="padding-top: 30px">
+      <el-table
+        ref="multipleTable"
+        :data="tableData3"
+        tooltip-effect="dark"
+        align="center"
+        height="650"
+        @selection-change="handleSelectionChange">
+        <el-table-column
+          type="selection"
+          width="100">
+        </el-table-column>
+        <el-table-column
+          prop="id"
+          label="id"
+          width="220">
+        </el-table-column>
+        <el-table-column
+          label="创建日期"
+          width="220">
+          <template slot-scope="scope">{{ scope.row.data }}</template>
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="220">
+        </el-table-column>
+        <el-table-column
+          prop="state"
+          label="是否锁定"
+          width="220">
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          width="100">
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--<div style="margin-top: 20px">-->
+      <!--<el-button @click="toggleSelection([tableData3[1], tableData3[2]])">切换第二、第三行的选中状态</el-button>-->
+      <!--<el-button @click="toggleSelection()">取消选择</el-button>-->
+      <!--</div>-->
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000" align="right" style="padding-top: 30px">
+      </el-pagination>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  mounted: function () {
+    // GET /someUrl
+    this.$http.get('http://localhost:8081/').then(response => {
+      console.log(response.data)
+      this.tableData3 = response.data
+    },
+    response => {
+      console.log('error')
+    })
+  },
+  data () {
+    return {
+      openeds: ['2'],
+      tableData3: this.tableData3,
+      multipleSelection: [],
+      isCollapse: false
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    toggleSelection (rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row)
+        })
+      } else {
+        this.$refs.multipleTable.clearSelection()
+      }
+    },
+    handleSelectionChange (val) {
+      this.multipleSelection = val
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
