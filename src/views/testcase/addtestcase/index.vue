9754d7ce7eb7a3460b4482c01a70b90a5f887596
w1q2q3q <1935417616@qq.com>
2018-11-09 15:30:20 +0800

    212

--- src/views/testcase/addtestcase/index.vue
+++ src/views/testcase/addtestcase/index.vue
@@ -4,38 +4,38 @@
      <el-tabs v-model="activeName" @tab-click="handleClick">
        <el-tab-pane label="接口信息管理" name="first">
          <el-form-item label="用例名称：" :label-width="formLabelWidth">
-           <el-input v-model="form.name" autocomplete="off" style="width: 350px"></el-input>
+           <el-input v-model="form.Cassname" autocomplete="off" style="width: 350px"></el-input>
          </el-form-item>
          <el-form-item label="用例描述：" :label-width="formLabelWidth">
-           <el-input v-model="form.describe" autocomplete="off" style="width: 350px"></el-input>
+           <el-input v-model="form.Cassdescribe" autocomplete="off" style="width: 350px"></el-input>
          </el-form-item>
          <el-form-item label="所属模块：" :label-width="formLabelWidth">
-           <el-select v-model="form.region" placeholder="请选择模块">
-             <el-option label="区域一" value="shanghai"></el-option>
-             <el-option label="区域二" value="beijing"></el-option>
+           <el-select v-model="form.Cassmodel" placeholder="请选择模块">
+             <el-option label="POST"  value="0"></el-option>
+             <el-option label="GET" value="1"></el-option>
            </el-select>
          </el-form-item>
        </el-tab-pane>
        <el-tab-pane label="配置管理" name="second">
          <el-form-item label="请求接口：" :label-width="formLabelWidth">
-           <el-input v-model="form.intertext" autocomplete="off" style="width: 350px"></el-input>
-           <el-select v-model="form.methodc" placeholder="请求方式">
-             <el-option label="区域一" value="shanghai"></el-option>
-             <el-option label="区域二" value="beijing"></el-option>
+           <el-input v-model="form.Url" autocomplete="off" style="width: 350px"></el-input>
+           <el-select v-model="form.Method" placeholder="请求方式">
+             <el-option label="POST" value="0"></el-option>
+             <el-option label="GET" value="1"></el-option>
            </el-select>
          </el-form-item>
          <el-form-item label="信息头：" :label-width="formLabelWidth">
-           <el-radio-group v-model="form.radio1">
+           <el-radio-group v-model="form.Herder">
              <el-radio label="0">无</el-radio>
              <el-radio label="1">添加herder</el-radio>
            </el-radio-group>
-           <div v-if="form.radio1==1">
+           <div v-if="form.Herder==1">
              <el-button size="small" type="primary" icon="el-icon-circle-plus-outline" @click="addTemp">新增信息头</el-button>
              <br/>
-            <el-table class="template-list" :data="form.tempList"  v-model="form.tempList" style="width: 70%">
+            <el-table class="template-list" :data="form.HerderList"  v-model="form.HerderList" style="width: 70%">
              <el-table-column label="信息头">
                <div slot-scope="scope">
-                 <el-select v-model="scope.row.pre" placeholder="信息头类型">
+                 <el-select v-model="scope.row.name" placeholder="信息头类型">
                    <el-option v-for="item in imageType" :key="item.value" :label="item.label" :value="item.value">
                    </el-option>
                  </el-select>
@@ -43,7 +43,7 @@
              </el-table-column>
              <el-table-column label="参数">
                <div slot-scope="scope">
-                 <el-input v-model="scope.row.name" placeholder="请填写参数"></el-input>
+                 <el-input v-model="scope.row.paramter" placeholder="请填写参数"></el-input>
                </div>
              </el-table-column>
              <el-table-column label="操作">
@@ -56,18 +56,18 @@
            </div>
          </el-form-item>
          <el-form-item label="参数：" :label-width="formLabelWidth">
-           <el-radio-group v-model="form.radio">
+           <el-radio-group v-model="form.Parameter">
              <el-radio label="0">无</el-radio>
              <el-radio label="1">List</el-radio>
              <el-radio label="2">Json</el-radio>
            </el-radio-group>
-           <div v-if="radio==1">
+           <div v-if="form.Parameter==1">
              <el-button size="small" type="primary" icon="el-icon-circle-plus-outline" @click="addTemp1">新增参数</el-button>
              <br/>
-             <el-table class="template-list" :data="form.tempList1"  style="width: 70%">
+             <el-table class="template-list" :data="form.ParameterList"  v-model="form.ParameterList" style="width: 70%">
                <el-table-column label="类型">
                  <div slot-scope="scope1">
-                   <el-select v-model="scope1.row.pre" placeholder="类型">
+                   <el-select v-model="scope1.row.name" placeholder="类型">
                      <el-option v-for="item in imageType2" :key="item.value" :label="item.label" :value="item.value">
                      </el-option>
                    </el-select>
@@ -75,7 +75,7 @@
                </el-table-column>
                <el-table-column label="参数">
                  <div slot-scope="scope1">
-                   <el-input v-model="scope1.row.name" placeholder="请填写参数"></el-input>
+                   <el-input v-model="scope1.row.paramter" placeholder="请填写参数"></el-input>
                  </div>
                </el-table-column>
                <el-table-column label="操作">
@@ -90,15 +90,17 @@
        </el-tab-pane>
        <el-tab-pane label="断言" name="third">
          <el-form-item label="断言：" :label-width="formLabelWidth">
-           <el-radio v-model="radio2" label="0">无</el-radio>
-           <el-radio v-model="radio2" label="1">添加断言</el-radio>
-           <div v-if="radio2==1">
+           <el-radio-group v-model="form.CassAssert">
+             <el-radio label="0">无</el-radio>
+             <el-radio label="1">添加断言</el-radio>
+           </el-radio-group>
+           <div v-if="form.CassAssert==1">
              <el-button size="small" type="primary" icon="el-icon-circle-plus-outline" @click="addTemp2">新增断言</el-button>
              <br/>
-             <el-table class="template-list" :data="tempList2"  style="width: 70%">
+             <el-table class="template-list" :data="form.AssertList"  v-model="form.AssertList" style="width: 70%">
                <el-table-column label="信息头">
                 <div slot-scope="scope2">
-                 <el-select v-model="scope2.row.pre" placeholder="信息头类型">
+                 <el-select v-model="scope2.row.name" placeholder="信息头类型">
                    <el-option v-for="item in imageType2" :key="item.value" :label="item.label" :value="item.value">
                    </el-option>
                  </el-select>
@@ -106,7 +108,7 @@
                </el-table-column>
                <el-table-column label="参数">
                  <div slot-scope="scope2">
-                   <el-input v-model="scope2.row.name" placeholder="请填写参数"></el-input>
+                   <el-input v-model="scope2.row.paramter" placeholder="请填写参数"></el-input>
                  </div>
                </el-table-column>
                <el-table-column label="操作">
@@ -119,7 +121,8 @@
            </div>
          </el-form-item>
        </el-tab-pane>
-       <el-tab-pane label="结果" name="fourth">定时任务补偿</el-tab-pane>
+       <el-tab-pane label="结果" name="fourth"></el-tab-pane>
+       <el-tab-pane label="数据提取" name="list"></el-tab-pane>
      </el-tabs>
      <el-button type="primary" @click="onSubmit">立即创建</el-button>
   </el-form>
@@ -133,16 +136,19 @@ export default {
       activeName: 'first',
       dialogFormVisible: false,
       form: {
-        name: '',
-        region: '',
-        methodc: '',
-        intertext: '',
-        radio1: '0',
-        tempList: [],
-        tempList1: []
+        Cassname: '',
+        Cassdescribe: '',
+        Url: '',
+        Cassmodel: '0',
+        Method: '0',
+        HerderList: [],
+        ParameterList: [],
+        AssertList: [],
+        Herder: '0',
+        Parameter: '0',
+        CassAssert: '0'
       },
       formLabelWidth: '120px',
-      tempList2: [],
       imageType: [{
         label: 'Cookies',
         value: 'Cookies'
@@ -150,52 +156,47 @@ export default {
       imageType2: [{
         label: 'Cookies',
         value: 'Cookies'
-      }],
-      radio: '0',
-      radio2: '0'
+      }]
     }
   },
   methods: {
     addTemp () {
-      this.form.tempList.push({
+      this.form.HerderList.push({
         name: '',
-        pre: '',
-        next: ''
+        paramter: ''
       })
     },
     // 删除
     handleDelete ($index, row) {
-      this.form.tempList.splice($index, 1)
+      this.form.HerderList.splice($index, 1)
       this.$message({
         type: 'success',
         message: '删除成功!'
       })
     },
     addTemp1 () {
-      this.tempList1.push({
+      this.form.ParameterList.push({
         name: '',
-        pre: '',
-        next: ''
+        paramter: ''
       })
     },
     // 删除
     handleDelete1 ($index, row) {
-      this.tempList1.splice($index, 1)
+      this.form.ParameterList.splice($index, 1)
       this.$message({
         type: 'success',
         message: '删除成功!'
       })
     },
     addTemp2 () {
-      this.tempList2.push({
+      this.form.AssertList.push({
         name: '',
-        pre: '',
-        next: ''
+        paramter: ''
       })
     },
     // 删除
     handleDelete2 ($index, row) {
-      this.tempList2.splice($index, 1)
+      this.form.AssertList.splice($index, 1)
       this.$message({
         type: 'success',
         message: '删除成功!'
@@ -205,7 +206,7 @@ export default {
       console.log(tab, event)
     },
     onSubmit () {
-      this.$http.post('http://localhost:8080/', JSON.stringify(this.form)).then(response => {
+      this.$http.post('http://localhost:8081/TestCass/addCass', JSON.stringify(this.form)).then(response => {
         console.log(response.data)
         this.tableData3 = response.data
       },
